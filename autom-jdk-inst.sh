73c5fd1a1d9d13628ffed9dbd3930508bfe54083
Pankaj Labade <pankajlabade99@gmail.com>
2014-02-19 15:37:03 +0530

    Update autom-jdk-inst.sh

--- autom-jdk-inst.sh
+++ autom-jdk-inst.sh
@@ -3,46 +3,42 @@
 # Author: Pankaj Labade
 
 # source the properties:
-. jdk-inst.properties
+. ./jdk-inst.properties
 
 echo JDK installer is present at $oracle_jdk_loc
 
 ext=${oracle_jdk_installer#*.}
+
+set_java(){
+   sudo update-alternatives --install "/usr/bin/java" "java" "$JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/java" 1 
+   sudo update-alternatives --install "/usr/bin/javac" "javac" "$JAVA_HOME_Parent_Dir/$oracle_jdk_val/bin/javac" 1 
+   sudo update-alternatives --install "/usr/bin/javaws" "javaws" "$JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/javaws" 1 
+    
+   # Run below commands to active the oracle JDK I just installed.    
+   sudo update-alternatives --set java $JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/java
+   sudo update-alternatives --set javac $JAVA_HOME_Parent_Dir/$oracle_jdk_val/bin/javac
+   sudo update-alternatives --set javaws $JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/javaws
+   
+   sudo rm -rf $JAVA_HOME_Parent_Dir/$oracle_jdk_installer
+}
+
 if [ $ext = "bin" ] ; then
-    echo You are providing a .bin file 
+    echo You are providing a $oracle_jdk_installer file 
     cd $oracle_jdk_loc
     sudo cp -rvf $oracle_jdk_loc/$oracle_jdk_installer $JAVA_HOME_Parent_Dir
     cd $JAVA_HOME_Parent_Dir
     sudo chmod +x $oracle_jdk_installer
     sudo ./$oracle_jdk_installer
+    set_java
 elif [ $ext = "tar.gz" ] ; then
-    echo You are providing a tar.gz file
+    echo You are providing a $oracle_jdk_installer file
     cd $oracle_jdk_loc
     sudo cp -rvf $oracle_jdk_loc/$oracle_jdk_installer $JAVA_HOME_Parent_Dir
     cd $JAVA_HOME_Parent_Dir
     sudo chmod +x $oracle_jdk_installer
     sudo tar xvzf $oracle_jdk_installer   
-elif [ $ext = "rpm" ] ; then
-    echo You are providing a tar.gz file
-    cd $oracle_jdk_loc
-    sudo cp -rvf $oracle_jdk_loc/$oracle_jdk_installer $JAVA_HOME_Parent_Dir
-    cd $JAVA_HOME_Parent_Dir
-    sudo apt-get install alien dpkg-dev debhelper build-essential
-    #Now convert package from RPM format to Deb format
-    sudo alien $oracle_jdk_installer
-    installer=${oracle_jdk_installer%%.*}.deb
-    sudo chmod +x $installer
-    sudo dpkg -i $installer
+    set_java
 fi
-
-sudo update-alternatives --install "/usr/bin/java" "java" "$JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/java" 1 
-sudo update-alternatives --install "/usr/bin/javac" "javac" "$JAVA_HOME_Parent_Dir/$oracle_jdk_val/bin/javac" 1 
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "$JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/javaws" 1 
-    
-# Run below commands to active the oracle JDK I just installed.    
-sudo update-alternatives --set java $JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/java
-sudo update-alternatives --set javac $JAVA_HOME_Parent_Dir/$oracle_jdk_val/bin/javac
-sudo update-alternatives --set javaws $JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/bin/javaws
 
 # Finally test the switch has been successful:
 java -version 
@@ -50,13 +46,15 @@ javac -version
 
 # Installing the Firefox/Chrome plugin
 
-mkdir ~/.mozilla/plugins
+if [ ! -d ~/.mozilla/plugins ]; then
+   mkdir ~/.mozilla/plugins
+fi
 
 # Remove the IcedTea plugin, if it has been installed.
-sudo apt-get remove icedtea6-plugin
+sudo apt-get -mf remove icedtea6-plugin
 
 # Remove a former version of the Java plugin (may or may not be present)
-rm ~/.mozilla/plugins/libnpjp2.so
+rm -fr ~/.mozilla/plugins/libnpjp2.so
 
 # Now you can install the plugin, by creating a symbolic link 
 # (you tell Firefox, where the plugin is located).
@@ -66,4 +64,3 @@ if [ "$JDK_32_bit" = "yes" ]; then
 else
     ln -s $JAVA_HOME_Parent_Dir/$oracle_jdk_val/jre/lib/amd64/libnpjp2.so ~/.mozilla/plugins/
 fi
-
